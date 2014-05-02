c7ce170ec4d0f71cc9dda91971f33fddb98d915f
npineau <nofigh@hotmail.fr>
2014-05-02 15:40:53 +0200

    updated strjoin, corrected strnew

--- sources/string/ft_strjoin.c
+++ sources/string/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/28 15:03:39 by npineau           #+#    #+#             */
-/*   Updated: 2014/02/05 12:06:13 by npineau          ###   ########.fr       */
+/*   Updated: 2014/05/02 15:31:49 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	char	*new;
 	int		len;
 
-	if (!(s1 && s2))
-		return (NULL);
 	len = ft_strlen(s1);
-	new = ft_strnew(len + ft_strlen(s2) + 1);
+	new = ft_strnew(len + ft_strlen(s2));
 	if (!new)
 		return (NULL);
 	ft_strcpy(new, s1);
--- sources/string/ft_strnew.c
+++ sources/string/ft_strnew.c
@@ -6,18 +6,24 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/26 12:49:33 by npineau           #+#    #+#             */
-/*   Updated: 2014/02/05 12:07:48 by npineau          ###   ########.fr       */
+/*   Updated: 2014/05/02 15:17:12 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
+/*
+** Utility allocating new array of char, adding space for the terminating byte.
+** It clears the memory allocated so that no garbage can parasite the calling
+** process.
+*/
+
 char	*ft_strnew(size_t size)
 {
 	char	*str;
 
-	str = malloc(sizeof(char *) * (size + 1));
+	str = (char *)malloc(sizeof(char) * (size + 1));
 	if (str)
 	{
 		size++;
