29a7af2de3ea317c67e6fa15e8eb4e8ed8926344
Shakadak <Shakadak@users.noreply.github.com>
2014-05-02 16:01:15 +0200

    Update ft_strtrim.c

--- sources/string/ft_strtrim.c
+++ sources/string/ft_strtrim.c
@@ -29,9 +29,9 @@ char		*ft_strtrim(const char *src)
 	l -= i;
 	if (l < 0)
 		l = 0;
-	new = ft_strnew(l + 1);
+	new = ft_strnew(l);
 	if (!new)
 		return (NULL);
-	new = ft_strncpy(new, &src[i], l);
+	new = ft_strncpy(new, src + i, l);
 	return (new);
 }
