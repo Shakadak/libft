3c00755da972fecfb5fa171b85a4f75441f6feb0
npineau <nofigh@hotmail.fr>
2014-05-23 12:58:02 +0200

    anti segfualt ?

--- sources/memory/ft_array_free.c
+++ sources/memory/ft_array_free.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/23 12:14:47 by npineau           #+#    #+#             */
-/*   Updated: 2014/05/23 12:22:42 by npineau          ###   ########.fr       */
+/*   Updated: 2014/05/23 12:57:35 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	ft_array_free(void ***array)
 		ft_memdel(*array);
 		(*array)++;
 	}
-	ft_memdel(*array);
+	*array = NULL;
 }
