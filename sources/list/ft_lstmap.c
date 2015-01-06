e0b82bea816e2cc602e39daf650f98441a66b6d1
npineau <npineau@student.42.fr>
2015-01-06 17:41:16 +0100

    Updating ft_lstmap

--- sources/list/ft_lstmap.c
+++ sources/list/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/30 16:48:17 by npineau           #+#    #+#             */
-/*   Updated: 2014/05/06 13:02:20 by npineau          ###   ########.fr       */
+/*   Updated: 2015/01/06 17:37:12 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,23 @@ static void	ft_elemdel(void *content, size_t size)
 t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new;
+	t_list	*mod;
 
 	new = NULL;
 	if (lst)
 	{
-		new = ft_lstnew((f(lst))->content, lst->content_size);
-		if (!new)
-			return (NULL);
-		if (lst->next)
+		mod = f(lst);
+		if (mod)
 		{
-			new->next = ft_lstmap(lst->next, f);
-			if (new->next == NULL)
-				ft_lstdel(&new, &ft_elemdel);
+			new = ft_lstnew(mod->content, mod->content_size);
+			if (!new)
+				return (NULL);
+			if (lst->next)
+			{
+				new->next = ft_lstmap(lst->next, f);
+				if (new->next == NULL)
+					ft_lstdel(&new, &ft_elemdel);
+			}
 		}
 	}
 	return (new);
