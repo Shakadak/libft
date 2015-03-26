697fb2172d74ee30594d52fce7f2fd4581c12a9e
Shakadak <nathanael.pineau@gmail.com>
2015-03-26 13:46:44 +0100

    and the header that goes with it:

--- inc/mem.h
+++ inc/mem.h
@@ -3,13 +3,14 @@
 
 void				ft_freesplit(void **array);
 void				ft_bzero(void *src, size_t length);
-void				*ft_memchr(const void *ptr, int c, size_t n);
-int					ft_memcmp(const void *s1, const void *s2, size_t n);
-void				*ft_memcpy(void *s1, const void *s2, size_t n);
-void				*ft_memccpy(void *s1, const void *s2, int c, size_t n);
-void				*ft_memmove(void *dest, const void *src, size_t n);
+void				*ft_memchr(void const *ptr, int c, size_t n);
+int					ft_memcmp(void const *s1, void const *s2, size_t n);
+void				*ft_memcpy(void *s1, void const *s2, size_t n);
+void				*ft_memccpy(void *s1, void const *s2, int c, size_t n);
+void				*ft_memmove(void *dest, void const *src, size_t n);
 void				*ft_memset(void *dest, int c, size_t length);
 void				*ft_memalloc(size_t size);
 void				ft_memdel(void **ap);
+void				*ft_memdup(void const *src, size_t const size);
 
 #endif
