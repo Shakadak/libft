2fb4dbf77742abc1c0bbcc20036fe66db9243623
shakadak <nathanael.pineau@gmail.com>
2015-05-08 14:55:35 +0200

    norme

--- src/io/ft_putendl.c
+++ src/io/ft_putendl.c
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/29 11:40:15 by npineau           #+#    #+#             */
-/*   Updated: 2014/02/05 12:03:47 by npineau          ###   ########.fr       */
+/*   Updated: 2015/05/08 14:55:00 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_putendl(const char *str)
 	if (ret == -1)
 		return (-1);
 	retn = ft_putchar('\n');
-	if ( retn == -1)
+	if (retn == -1)
 		return (-1);
 	return (ret + retn);
 }
