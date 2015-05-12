abe3d210af1b231ccd7593a9a4a8ba4163bc37b2
shakadak <nathanael.pineau@gmail.com>
2015-05-12 17:21:48 +0200

    adding missing prototypes

--- inc/rb.h
+++ inc/rb.h
@@ -10,7 +10,13 @@ typedef struct	s_rb
 	size_t		tail;
 }				t_rb;
 
+size_t			rb_consume(t_rb *const src, void *dest, size_t n);
+size_t			rb_cpymem(t_rb *const src, void *dest, size_t n);
 t_rb			rb_dup(t_rb const src);
+int				rb_empty(t_rb const buff);
+void			rb_free(t_rb * const rb);
+int				rb_full(t_rb const buff);
 t_rb			rb_new(size_t const length);
+size_t			rb_produce(void *const src, t_rb *dest, size_t const n);
 
 #endif
