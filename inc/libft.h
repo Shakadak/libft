79e0d5af1ed1e8a6b2cab881b9e0a494439d7ac1
Nathanael PINEAU <npineau@student.42.fr>
2017-11-29 15:36:04 +0100

    norme cleanup also, I don't love the C language, just to be sure

--- inc/libft.h
+++ inc/libft.h
@@ -6,14 +6,13 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/20 11:00:59 by npineau           #+#    #+#             */
-/*   Updated: 2017/11/29 10:15:19 by npineau          ###   ########.fr       */
+/*   Updated: 2017/11/29 15:35:16 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-//# include <string.h>
 # include "arr.h"
 # include "char.h"
 # include "io.h"
